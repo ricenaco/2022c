d1e47cc9cfca9156c5129d82923335e9f58ed6cc
ricenaco <hmikeank@gmail.com>
2022-12-02 12:03:00 +0800

    week13

---
+++ week13/week13-1.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int a[100];
+int main()
+
+{
+	int n;
+	scanf("%d",&n);
+	for (int i=0;i<n;i++)
+		scanf("%d",&a[i]);
+
+	int l,r;
+	int ans = -99999999 ;
+
+	for (int i=0;i<n;i++)
+	{
+		for (int j=i+1;j<n;j++)
+		{
+			if(a[j]-a[i]>ans)
+			{
+				ans=a[j]-a[i];
+				l=a[i];
+				r=a[j];
+			}
+		}
+	}
+
+	printf("請按任意鍵繼續 . . . \n");
+	printf("最大利潤=%d-%d=%d\n",r,l,ans);
+}
